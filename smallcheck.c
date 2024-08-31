f84d08e25ceaecfcccb3fbb46a14f8e2042c3e76
SANAPALASANJAY <lairsup090@gmail.com>
2024-08-31 17:58:14 +0530

    smallcheck.c

--- smallcheck.c
+++ smallcheck.c
@@ -5,7 +5,7 @@ int main()
     char a;
     printf("Enter the character : ");
     scanf("%c",&a);
-    
+    //checks if the character is small case or not 
     int c = a;
     c>=97 && c<=122 ? printf("YES"):printf("NO");
     return 0;
