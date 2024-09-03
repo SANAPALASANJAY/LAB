fbf507359c0f0648dea969af3c9affc148bfae55
SANAPALASANJAY <lairsup090@gmail.com>
2024-09-03 13:43:36 +0530

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,n,x;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    for(int i = 1;i < n;i++)
+    {
+        if(n%i==0)
+        {
+            a++;
+        }
+    }
+    
+    if(a<=1)
+    {
+        printf("PRIME");
+    }
+    else
+    {
+        printf("NOT PRIME");
+    }
+    return 0;
+}
