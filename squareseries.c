a2061462c89d781a0e9c2d96a2f722b833bea199
SANAPALASANJAY <lairsup090@gmail.com>
2024-09-03 14:33:18 +0530

    Add files via upload

---
+++ squareseries.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,x;
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    
+    for( int i = 1;i <= n; i++)
+    {
+        x = pow(i,2);
+        printf("%d ",x);
+    }
+    return 0;
+}
