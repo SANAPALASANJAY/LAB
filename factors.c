3c8db4c71da581e8ea7d4951e566a2a0943d1bca
SANAPALASANJAY <lairsup090@gmail.com>
2024-09-03 14:42:53 +0530

    Add files via upload

---
+++ factors.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    
+    for( int i = n;i >= 1; i--)
+    {
+        if(n%i ==0)
+        {
+            printf("%d ",n/i);
+        }
+    }
+    return 0;
+}
