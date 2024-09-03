20f8b88e87aa5aea89039319bd55e2a0444fbd39
SANAPALASANJAY <lairsup090@gmail.com>
2024-09-03 22:10:09 +0530

    sumoflist.c

---
+++ sumoflist_untilnegative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n,sum=0;
+    do
+    {
+        printf("Enter a number: ");
+        scanf("%d",&n);
+        if(n >= 0)
+        {
+            sum = sum + n;
+        }
+    }while(n>0);
+    printf("%d",sum);
+    return 0;
+}
