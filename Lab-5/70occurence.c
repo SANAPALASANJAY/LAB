6462060de8148524d3f58c99b8a4b5f1c7055535
SANAPALASANJAY <lairsup090@gmail.com>
2024-09-24 21:39:00 +0530

    Create 70occurence.c

---
+++ Lab-5/70occurence.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n,e,flag=0;
+    printf("Enter no.of elements: ");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter elements:\n");
+    for(int i = 0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    printf("Enter the element to search:");
+    scanf("%d",&e);
+    for(int i = 0;i<n;i++)
+    {
+        if(a[i]==e)
+        {
+            printf("index:%d\n",i);
+            flag++;
+        }
+    }
+    printf("occurence : %d",flag);
+    
+    return 0;
+}
