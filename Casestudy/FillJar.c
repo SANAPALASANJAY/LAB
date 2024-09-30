577d5d688b4faef9afab2fa0d170f24bba21af46
SANAPALASANJAY <lairsup090@gmail.com>
2024-09-30 23:54:02 +0530

    Update FillJar.c

--- Casestudy/FillJar.c
+++ Casestudy/FillJar.c
@@ -3,10 +3,14 @@
 int main()
 {
     int n,m,sum=0;
+    do{
     printf("Enter the number of jars : ");
     scanf("%d",&n);
+    }while(n<3 || n>100);
+    do{
     printf("Enter the number of operations : ");
     scanf("%d",&m);
+    }while(m<1);
     printf("operation syntax : lower-limit upper-limit value-to-increase\n");
     int jars[n];
     for(int i = 0; i<n;i++)//setting the values of all jars to zero
@@ -16,7 +20,10 @@ int main()
     for(int i = 0;i < m;i++)
     {
         int a,b,value;//a= lwrlmt,b=uprlmt,value= value to add 
+        do{
+            printf("Enter operation %d : ",i + 1);
         scanf("%d %d %d",&a,&b,&value);
+        }while(a<1 || b>n);
         for(int j = a-1;j<b;j++)
         {
             jars[j] = jars[j] + value;
@@ -31,5 +38,4 @@ int main()
     float avg = ( (float) sum / n );
     printf("Average = %f",avg);
     return 0;
-    
 }
