25507c79aa6ec29938781127ba14a322f3af3b75
SANAPALASANJAY <lairsup090@gmail.com>
2024-10-06 10:20:19 +0530

    Create 79exchange.c

---
+++ Lab-7/79exchange.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void exchange(int *x,int *y);
+int main() {
+    int x,y;
+    printf("Enter x: ");
+    scanf("%d",&x);
+    printf("Enter y: ");
+    scanf("%d",&y);
+    exchange(&x,&y);
+    printf("After Exchanging\n");
+    printf("x: %d y: %d",x,y);
+    return 0;
+}
+void exchange(int *x,int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
