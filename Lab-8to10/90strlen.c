6d5ffd6238399379314e474c6ea6fc1da3d2ba62
SANAPALASANJAY <lairsup090@gmail.com>
2024-11-14 15:41:28 +0530

    Create 90strlen.c

---
+++ Lab-8to10/90strlen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char* text;
+    int lwiths = 0, lwithouts = 0, i = 0;
+    text = (char*)malloc(200*sizeof(char));
+    fgets(text, 200, stdin);
+    
+    while(*(text + i) != '\n')
+    {
+        if(*(text + i) == ' ')
+        {
+            lwiths++;
+        }
+        else
+        {
+            lwiths++;
+            lwithouts++;
+        }
+        i++;
+    }
+    
+    printf("length with spaces: %d\n",lwiths);
+    printf("length wihtout spaces: %d", lwithouts);
+    
+    return 0;
+}
