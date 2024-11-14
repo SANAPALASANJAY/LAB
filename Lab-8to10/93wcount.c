dd3a601bc95e99b20c1b1664faa6cfd2e05c1d80
SANAPALASANJAY <lairsup090@gmail.com>
2024-11-14 21:15:33 +0530

    Create 93wcount.c

---
+++ Lab-8to10/93wcount.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char* str;
+    int words = 1;
+    str = (char*)malloc(200 * sizeof(char));
+    fgets(str, 200, stdin);
+
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (*(str + i) == ' ')
+        {
+            words++;
+        }
+    }
+
+    printf("No.of Words = %d\n", words);
+    return 0;
+}
