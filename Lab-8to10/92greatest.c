cde752601f5512760feb0926bbc766be302487a7
SANAPALASANJAY <lairsup090@gmail.com>
2024-11-14 21:14:57 +0530

    Create 92greatest.c

---
+++ Lab-8to10/92greatest.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,greatest;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int* a;
+    printf("Enter the elements\n");
+    a = (int*)malloc(n * sizeof(int));
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", (a + i));
+    }
+    greatest = a[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (greatest < *(a + i))
+        {
+            greatest = *(a + i);
+        }
+    }
+
+    printf("Greatest: %d\n", greatest);
+    return 0;
+}
