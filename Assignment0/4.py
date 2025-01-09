8f77ee54cf3e81203b84e4971980eb93d607b492
SANAPALASANJAY <lairsup090@gmail.com>
2025-01-09 14:20:26 +0530

    Create 4.py

---
+++ Assignment0/4.py
@@ -0,0 +1,7 @@
+a = int(input("Enter number a: "))
+b = int(input("Enter number b: "))
+#swapping
+a = a + b 
+b = a - b # a + b - b
+a = a - b # a + b - a(b)
+print("swapped a = %d , b = %d" %(a, b))
