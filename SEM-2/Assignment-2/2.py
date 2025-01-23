ed5604d76c0e0a3c88cb484c5107f9f5aecc4f08
SANAPALASANJAY <lairsup090@gmail.com>
2025-01-23 14:53:47 +0530

    Create 2.py

---
+++ SEM-2/Assignment-2/2.py
@@ -0,0 +1,28 @@
+Products = {}
+print("ENTER THE DATA OF PRODUCTS")
+print("Type enq to go to the product enquiry OR\n")
+
+while True:
+    Name = input("Enter the product name: ")
+    if Name == "enq":
+        break
+    else:
+        try:
+            val = float(input("Enter the price of product: "))
+            Products[Name] = val
+        except ValueError:
+            print("Invalid Price. Please enter a number")
+
+print("\nPRODUCT ENQUIRY")
+print("Enter exit if you want to exit OR")
+
+while True:
+    Name = input("Enter the product name: ")
+    if Name == "exit":
+        break
+    if Name in Products:
+        print("Price: %d"%(Products[Name]))
+    else:
+        print("Product is not available")
+
+print("Thank You")
